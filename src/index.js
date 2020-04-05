c08d8e211dae45a0249c2178eece2e219b0a51b1
Mehulcoder <mehul355180@gmail.com>
2020-04-05 14:59:42 +0530

    Made designer Model

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ var app = express();
 var port = 3000 || process.env.PORT;
 
 app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
+app.use(routers);
 
 app.listen(port, () => {
     console.log("Server is on port " + port);
---
+++ src/models/designer.js
@@ -0,0 +1,79 @@
+var mongoose = require('mongoose');
+var validator = require('validator');
+var bcrypt = require('bcryptjs');
+var sharp = require('sharp');
+
+//
+// ─── DESIGNER MODEL SCHEMA ──────────────────────────────────────────────────────
+//
+
+var designerSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        lowercase:true,
+        unique:true,
+        validate(value){
+            if (!validator.isEmail(value)) {
+                throw new Error("Please enter a valid E-mail!");
+            }
+        }
+    },
+    password:{
+        type:String,
+        required:true,
+        validate(value){
+            if(!validator.isLength(value,{min:6,max:1000}))
+            {
+                throw Error("Minimum length of the password must be 6");
+            }
+
+            if(value.toLowerCase().includes('password')){
+                throw Error('The password should not contain the keyword "password"!');
+            }
+        }
+
+    },
+    capacity:{
+        type:Number,
+        required:true,
+        trim:true,
+        validate(value){
+            if (value<0) {
+                throw new Error("Capacity must be a positive number");
+            }
+        }
+    },
+    category:{
+        type: String,
+        required:true,
+        trim:true,
+        lowercase:true,
+        enum:{
+            values:[furniture designer,architect,interior designer,industrial designer,designer maker,other],
+            message: 'Please choose from the given options only!'
+        }
+    },
+    training:{
+        type:String
+        type: String,
+        required:true,
+        trim:true,
+        lowercase:true
+    }
+})
+
+//
+// ─── MODEL THE SCHEMA ─────────────────────────────────────────────────────────────
+//    
+
+var Designer = mongoose.model('Designer', designerSchema);
+
+//
+// ─── MODULE EXPORT ──────────────────────────────────────────────────────────────
+//
+
+module.exports = Designer;
+
+// ────────────────────────────────────────────────────────────────────────────────
